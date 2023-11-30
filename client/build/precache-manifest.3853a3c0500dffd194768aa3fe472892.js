92111aa44fae5fd37522f3d991cf2e46fdc3b7f7
tmyn <tmyn@example.com>
2023-11-30 03:45:17 -0500

    removed /build in client/gitignore to give out the client/build folder to be published on netlify

---
+++ client/build/precache-manifest.3853a3c0500dffd194768aa3fe472892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0cd43bdaaf864e8550ac208d6a8b305",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76a49d1ffcb26efa2eab",
+    "url": "/static/css/main.176aaa8d.chunk.css"
+  },
+  {
+    "revision": "7a298d15de869757c1ca",
+    "url": "/static/js/2.d137e858.chunk.js"
+  },
+  {
+    "revision": "3f1172a4ab9908da89f09486365a8f0c",
+    "url": "/static/js/2.d137e858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a49d1ffcb26efa2eab",
+    "url": "/static/js/main.274df5ef.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
